1d93c2ace48ec4d0c2a11eb6b5d8694f5eb54fc7
behniyaesm <behniyaesm@gmail.com>
2024-09-19 13:48:01 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double purchasePrice = 350.50;
+        double salesTax = 0 ;
+        final double SALES_TAX_RATE = 0.5;  //use final to make it a constant
+
+        salesTax = purchasePrice * SALES_TAX_RATE;
+
+        System.out.println("The sales tax on a purchase of " + purchasePrice + "is $" + salesTax);
+    }
+}
