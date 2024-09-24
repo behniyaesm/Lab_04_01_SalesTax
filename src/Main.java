2dd718d52d07d89226f73bda7ab453720c5ada89
behniyaesm <behniyaesm@gmail.com>
2024-09-23 21:03:20 -0400

    Completed task 1

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,12 @@ public class Main
     {
         double purchasePrice = 350.50;
         double salesTax = 0 ;
-        final double SALES_TAX_RATE = 0.5;  //use final to make it a constant
+        double totalCost = 0 ;
+        final double SALES_TAX_RATE = 0.05;  //use final to make it a constant
 
         salesTax = purchasePrice * SALES_TAX_RATE;
-
+        totalCost = purchasePrice + salesTax;
         System.out.println("The sales tax on a purchase of " + purchasePrice + " is $" + salesTax);
+        System.out.println("The total price on a item that costs $" + purchasePrice + " is $" + totalCost + " with sales tax.");
     }
 }
